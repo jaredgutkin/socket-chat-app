a0f337dc093f25ddbd1b52c14b2e16b01ff6a5ef
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-26 20:44:29 -0500

    server side socket io set up

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require('express')
+const io = require('socket.io')(http)
 const PORT = process.env.PORT || 3000
 const app = express()
 const http = require('http').Server(app)
 
+//middleware
 app.use(express.static(__dirname + '/public'))
 
+//serves up home page
 app.get('/', (req, res)=>{
     res.sendFile('index.html')
 })
 
+
+io.on('connection', (socket)=>{
+    socket.on('user_join', (data)=>{
+        this.username = data
+        socket.broadcast.emit('user_join', data)
+    })
+
+    socket.on('chat_message', (data)=>{
+        data.username = this.username
+        socket.broadcast.emit('chat_message', data)
+    })
+
+    socket.on('disconnect', (data)=>{
+        socket.broadcast.emit('user_leave', this.username)
+    })
+})
+
+//connect to server
 http.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`)
 })
